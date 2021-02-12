5b50a04de95f04f52ed37a4c3dec4f8a8f93bc7b
wylie <wyliedavid1984@gmail.com>
2021-02-11 19:29:22 -0500

    commented out a route

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -4,28 +4,28 @@ const API_KEY = require("../.env")
 
 module.exports={ 
     
-    find: function(req, res) {
-        const {query: params } = req
-        axios.get("https://www.googleapis.com/books/v1/volumes?q="+params+"&key="+API_KEY)
-        .then(results => {
-           results.data.items.filter(
-               result =>
-               result.volumeInfo.title&&
-               result.volumeInfo.infoLink&&
-               result.volumeInfo.authors&&
-               result.volumeInfo.description&&
-               result.volumeInfo.imageLinks.thumbnail
-           )
-        })
-        .then(apiBooks =>{
-            db.Book.find().then(dbBooks =>
-            apiBooks.filter(apiBook => {
-                dbBooks.every(dbBook => dbBook.googleId.toString() !== apiBook.id)
-            }))
+    // find: function(req, res) {
+    //     const {query: params } = req
+    //     axios.get("https://www.googleapis.com/books/v1/volumes?q="+params+"&key="+API_KEY)
+    //     .then(results => {
+    //        results.data.items.filter(
+    //            result =>
+    //            result.volumeInfo.title&&
+    //            result.volumeInfo.infoLink&&
+    //            result.volumeInfo.authors&&
+    //            result.volumeInfo.description&&
+    //            result.volumeInfo.imageLinks.thumbnail
+    //        )
+    //     })
+    //     .then(apiBooks =>{
+    //         db.Book.find().then(dbBooks =>
+    //         apiBooks.filter(apiBook => {
+    //             dbBooks.every(dbBook => dbBook.googleId.toString() !== apiBook.id)
+    //         }))
             
-        })
-        .then(books => res.json(books))
-        .catch(err => console.log(err))
-    }
+    //     })
+    //     .then(books => res.json(books))
+    //     .catch(err => console.log(err))
+    // }
 }
 // https://www.googleapis.com/books/v1/volumes?q=search+terms
